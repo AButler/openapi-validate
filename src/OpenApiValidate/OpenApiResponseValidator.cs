34eef923b32e0bb92d158197c70b9ce25a476810
Andrew Butler <1628649+AButler@users.noreply.github.com>
2025-02-02 14:24:24 +0000

    Tidy solution

--- src/OpenApiValidate/OpenApiResponseValidator.cs
+++ src/OpenApiValidate/OpenApiResponseValidator.cs
@@ -177,7 +177,7 @@ public class OpenApiResponseValidator
     {
         validationErrors = [];
 
-        if (!operation.Responses.TryMatchResponse(response.StatusCode, out var responseModel))
+        if (operation.Responses == null || !operation.Responses.TryMatchResponse(response.StatusCode, out var responseModel))
         {
             validationErrors.Add(
                 new ValidationError(
